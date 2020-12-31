94718ae77bfa6817f43d7616796c6462b05e2e22
b562074b2c2c9847c0581168ed03fa5d44ceec1c <bzhang@centos1.lab.local>
2020-12-31 17:55:58 -0500

    another update

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 3 -- 20201231 GKE
+            Learn React 3 -- 20201231 GKE - v2
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+SHA=$(git rev-parse HEAD)
 docker build -t brucezw/multi-client:latest -t brucezw/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t brucezw/multi-server:latest -t brucezw/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t brucezw/multi-worker:latest -t brucezw/multi-worker:$SHA -f ./worker/Dockerfile ./worker
@@ -10,5 +11,5 @@ docker push brucezw/multi-worker:$SHA
 
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment  server=brucezw/multi-server:$SHA
-kubectl set image deployments/client-deployment  server=brucezw/multi-client:$SHA
-kubectl set image deployments/worker-deployment  server=brucezw/multi-worker:$SHA
+kubectl set image deployments/client-deployment  client=brucezw/multi-client:$SHA
+kubectl set image deployments/worker-deployment  worker=brucezw/multi-worker:$SHA
