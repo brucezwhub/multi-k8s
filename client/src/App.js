f75da5fea25215373f824c1dac0ebf4eb834a2c3
b562074b2c2c9847c0581168ed03fa5d44ceec1c <bzhang@centos1.lab.local>
2020-12-31 19:26:39 -0500

    dd

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 3 -- 20201231 GKE - v1
+            Learn React latest commit from git.
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
